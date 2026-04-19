b4305966effd1ddf47f88f42ee1a61299edd7af2
Lorenzo S. <46894435+Suffro@users.noreply.github.com>
2026-04-19 17:30:57 +0200

    Update function_examples.rs

--- module-template/src/function_examples.rs
+++ module-template/src/function_examples.rs
@@ -2,6 +2,9 @@
 
 use serde_json::{Value, json};
 
+
+// named arguments examples (eg. { "a": 3, "b": 5 }):
+
 pub fn eg_sum(args: &Value) -> Result<Value, String> {
     let a = args.get("a").and_then(|x| x.as_f64()).unwrap_or(0.0);
     let b = args.get("b").and_then(|x| x.as_f64()).unwrap_or(0.0);
@@ -11,4 +14,13 @@ pub fn eg_sum(args: &Value) -> Result<Value, String> {
 pub fn eg_greet(args: &Value) -> Result<Value, String> {
     let name = args.get("name").and_then(|x| x.as_str()).unwrap_or("world");
     Ok(json!(format!("Hello {}", name)))
+}
+
+
+// positional arguments example (eg. [3,5]):
+
+pub fn eg_sum_positional(args: &Value) -> Result<Value, String> {
+    let a = args.get(0).and_then(|x| x.as_f64()).unwrap_or(0.0);
+    let b = args.get(1).and_then(|x| x.as_f64()).unwrap_or(0.0);
+    Ok(json!(a + b))
 }
