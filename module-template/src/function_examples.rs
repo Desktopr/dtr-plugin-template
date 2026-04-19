1d1a95a2119f3dd4b55aba30cbf257a71f82d7cc
Lorenzo S. <46894435+Suffro@users.noreply.github.com>
2026-04-19 17:41:32 +0200

    Update function_examples.rs

--- module-template/src/function_examples.rs
+++ module-template/src/function_examples.rs
@@ -3,24 +3,39 @@
 use serde_json::{Value, json};
 
 
-// named arguments examples (eg. { "a": 3, "b": 5 }):
+// named arguments examples (eg. { "a": 16, "b": 2 }):
 
-pub fn eg_sum(args: &Value) -> Result<Value, String> {
+pub fn eg_divide(args: &Value) -> Result<Value, String> {
+
     let a = args.get("a").and_then(|x| x.as_f64()).unwrap_or(0.0);
     let b = args.get("b").and_then(|x| x.as_f64()).unwrap_or(0.0);
-    Ok(json!(a + b))
+    
+    if b == 0.0 {
+        return Err("division by zero".to_string());
+    }
+
+    Ok(json!(a / b))
 }
 
 pub fn eg_greet(args: &Value) -> Result<Value, String> {
+
     let name = args.get("name").and_then(|x| x.as_str()).unwrap_or("world");
+
     Ok(json!(format!("Hello {}", name)))
 }
 
 
-// positional arguments example (eg. [3,5]):
 
-pub fn eg_sum_positional(args: &Value) -> Result<Value, String> {
+// positional arguments example (eg. [16,2]):
+
+pub fn eg_divide_positional(args: &Value) -> Result<Value, String> {
+
     let a = args.get(0).and_then(|x| x.as_f64()).unwrap_or(0.0);
     let b = args.get(1).and_then(|x| x.as_f64()).unwrap_or(0.0);
-    Ok(json!(a + b))
+
+    if b == 0.0 {
+        return Err("division by zero".to_string());
+    }
+
+    Ok(json!(a / b))
 }
